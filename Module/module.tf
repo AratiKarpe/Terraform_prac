f0e3c240bcec811cb31ca4a78b82a581de5fa706
Arati Karpe <karpearati76@gmail.com>
2026-06-20 21:08:12 +0530

    sa

--- Module/module.tf
+++ Module/module.tf
@@ -1,9 +1,9 @@
 module "EC2" {
   source = "/home/arati_26/terraform/Resource/EC2/"            
-  this_key_name = "ohiophk" 
-  this_ami = "ami-0eab37cfdc33e8e65" 
+  this_key_name = "sample" 
+  this_ami = "ami-0ea1cddefe0c4aed5" 
   This_instance_type = "t3.micro"
-  #This_vpc_security_group_ids = "sg-0efc868be83f601ce" 
+  #This_vpc_security_group_ids = "sg-0c3878cd86dc51539" 
   this_count = 1 
   this_disable_api_termination = false
   this_subnetid = module.VPC.subnet_id

cf02e3b2c41665de9bbe03177311672b875dbf90
Arati Karpe <karpearati76@gmail.com>
2026-06-21 13:20:42 +0530

    sa

--- Resource/EC2/resource.tf
+++ Resource/EC2/resource.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "webserver" {
-   #depends_on = [aws_security_group.webserversg] #explict dependency
+   
    key_name = var.this_key_name
    ami = var.this_ami 
    instance_type = var.This_instance_type
-   vpc_security_group_ids = [var.This_vpc_security_group_ids  ]
+   
    disable_api_termination = var.this_disable_api_termination
    subnet_id = var.this_subnetid
    user_data = <<-EOF

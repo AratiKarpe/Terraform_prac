292bb67a7035c217b7dbdc85820b15875d512f07
Arati Karpe <karpearati76@gmail.com>
2026-06-21 13:33:54 +0530

    sa

--- Module/module.tf
+++ Module/module.tf
@@ -10,7 +10,7 @@ module "EC2" {
 }
 
 module "VPC" {
-    source = "/home/arati_26/terraform/Resource/EC2/"
+    source = "/home/arati_26/terraform/Resource/VPC/"
     thisvpc_cidr = "172.30.0.0/16"
     cbz_subneta = "172.30.0.0/20"
 }

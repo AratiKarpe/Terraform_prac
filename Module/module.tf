2fdebbd649769a724b662a853bc7ea6ff0391dd7
Arati Karpe <karpearati76@gmail.com>
2026-06-21 15:46:41 +0530

    sa

--- Module/module.tf
+++ Module/module.tf
@@ -3,7 +3,7 @@ module "EC2" {
   this_key_name = "sample" 
   this_ami = "ami-0ea1cddefe0c4aed5" 
   This_instance_type = "t3.micro"
-  This_vpc_security_group_ids = "sg-0c3878cd86dc51539" 
+  #This_vpc_security_group_ids = "sg-0c3878cd86dc51539" 
   this_count = 1 
   this_disable_api_termination = false
   this_subnetid = module.VPC.subnet_id
